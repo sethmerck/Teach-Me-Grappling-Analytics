1cd668da354e9da132dee05b20e5400ea6b3cfbc
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2022-12-18 20:20:06 -0500

    Update csv_df.py

--- csv_df.py
+++ csv_df.py
@@ -1,9 +1,10 @@
 import pandas as pd
-import re
 import matplotlib.pyplot as plt
 
+# create dataframe
 df = pd.read_csv('teach_me_grappling.csv')
 
+# turn viewcount datatype from string to integer
 def condition(x):
     if 'K' in x:
         return int(float(x.replace('K views', ''))*1000)
@@ -14,10 +15,18 @@ def condition(x):
 
 df.views = df['views'].apply(condition)
 
+# obtain categories
+grouped = df.groupby(df['title'].str.contains('escape', case=False))
+oth = df.groupby(df['title'].str.contains(''))
 
-print(df.views.mean(), df.views.median())
-print(df)
 
-plt.bar(['Mean Views', 'Median Views'], [int(df.views.mean()), int(df.views.median())])
-plt.title("Channel Videos' Mean and Median Views")
+# create bar plot using median value of each category
+median_views = pd.concat([grouped.median(),oth.median()],axis=0)
+median_views.plot(kind='bar')
+plt.xticks(rotation=0, ticks=[0,1,2], labels=['w/o "escape"','WITH "escape"', 'All'])
+plt.xlabel('Video Group')
+plt.ylabel('Median Views')
+legend = plt.legend()
+legend.remove()
+plt.title('Teach Me Grappling View Data')
 plt.savefig('barplot.png')
