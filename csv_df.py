d4aaffd48291891bd56fd67ccf3b83bebf9d9a4c
Seth Merck <113151310+sethmerck@users.noreply.github.com>
2022-12-18 21:40:05 -0500

    Update csv_df.py

--- csv_df.py
+++ csv_df.py
@@ -21,8 +21,17 @@ oth = df.groupby(df['title'].str.contains(''))
 
 
 # create bar plot using median value of each category
+counts = pd.concat([grouped.count(), oth.count()], axis=0)
+print([i for i in counts.title.values])
 median_views = pd.concat([grouped.median(),oth.median()],axis=0)
+med_views_list = [i for i in median_views.views.values]
 median_views.plot(kind='bar')
+
+data = [i for i in counts.title.values]
+
+for i, v in enumerate(data):
+    plt.text(i-0.225, med_views_list[i] * 0.5, f'n = {str(v)}', fontsize=12)
+
 plt.xticks(rotation=0, ticks=[0,1,2], labels=['w/o "escape"','WITH "escape"', 'All'])
 plt.xlabel('Video Group')
 plt.ylabel('Median Views')
